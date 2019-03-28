a6109b80c33ccdc8e2dde6d37159b788187909f4
ChaeSongi <new9605@naver.com>
2019-03-28 13:23:24 +0900

    for문

--- NadoCoding_2/rootlob.c
+++ NadoCoding_2/rootlob.c
@@ -5,10 +5,20 @@ int main(void)
 	/*printf("Hello World!");*/
 
 	// ++ 뿔뿔
-	int b = 20;
+	/*int b = 20;
 	printf("b 는 %d\n", b++);
 	printf("b 는 %d\n", ++b);
-	printf("b는 %d\n", b);
+	printf("b는 %d\n", b);*/
+
+	// 반복문 
+	// for, while, do while
+
+	//for (선언; 조건;증감)
+	for (int i = 1; i <= 10; i++)
+	{
+		printf("Hello World! %d\n ", i);
+	}
+
 	return 0;
 	
 }
